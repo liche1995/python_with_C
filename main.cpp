96c8d9378d3686fef01b3c343dfcf6d2d45eebe4
闕立奇 <liche0241@gmail.com>
2022-10-12 15:33:56 +0800

    edit task.json

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 #include <ctime>
 using namespace std;
 
+class person{
+    public:
+        string name;
+        int age;
+        float height;
+        float weight;
+        char gender[1];
+        void what_name();
+};
+
+void person::what_name(){
+    cout << "My name is "<< name << endl;
+}
+
+
 int main(){
     int x;
     time_t t_enity = time(0);
@@ -11,6 +26,12 @@ int main(){
     printf("hello\n");
     printf("%d\n", now->tm_mday);
     cout << x;
+    
+    person p1;
+    cin >> p1.name >> p1.age >> p1.height >> p1.weight >> p1.gender;
+
+    p1.what_name();
+
     system("pause");
     return 0;
 }
